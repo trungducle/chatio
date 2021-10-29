f7220ce9adc320e224be64df9f7cc5f7f1aed4f6
datnguyenxtiens <dat192001@gmail.com>
2021-10-29 20:46:57 +0700

    Minor fixes

--- client/src/components/chatMenu/Room.jsx
+++ client/src/components/chatMenu/Room.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./room.css";
 
 const Room = ({ id, name, lastMessage }) => {
-  const showedName = name.length > 30? name.substring(0,30) + "..." : name;
+  const showedName = name.length > 30? name.substring(0,28) + "..." : name;
   const showedLastMessage = lastMessage.length > 40? lastMessage.substring(0,40) + "..." : lastMessage;
 
   return (
