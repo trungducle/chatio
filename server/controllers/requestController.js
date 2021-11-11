8567c6c1ce97eda0725e6a38a85b55080f9165ac
datnguyenxtiens <dat192001@gmail.com>
2021-11-11 07:28:53 +0700

    Minor updates

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -7,14 +7,59 @@ exports.getFriendRequests = async (req, res) => {
     if (!isNaN(userId)) {
       console.log(id);
       const result = await db.any(
-        "SELECT concat_ws(' ', a.first_name, a.last_name) full_name, a.email\
+        "SELECT concat_ws(' ', a.first_name, a.last_name) full_name, a.email, pd.sender_id\
         FROM pending_request pd JOIN account a\
         ON pd.sender_id = a.user_id\
-        WHERE receiver_id=$1;",
+        WHERE pd.recipient_id=$1;",
         [userId]
       );
   
       res.status(200).json(result);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.rejectFriendRequest = async (req, res) => {
+  const { sender, recipient } = req.query;
+  try {
+    const senderId = parseInt(sender);
+    const recipientId = parseInt(recipient);
+
+    if (!isNaN(senderId) && !isNaN(recipientId)) {
+      await db.none(
+        "DELETE FROM pending_request\
+        WHERE sender_id=$1 AND recipient_id=$2",
+        [senderId, recipientId]
+      );
+  
+      res.status(200).send("Request rejected");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.acceptFriendRequest = async (req, res) => {
+  const { sender, recipient } = req.query;
+  try {
+    const senderId = parseInt(sender);
+    const recipientId = parseInt(recipient);
+
+    if (!isNaN(senderId) && !isNaN(recipientId)) {
+      await db.none(
+        "DELETE FROM pending_request\
+        WHERE sender_id=$1 AND recipient_id=$2",
+        [senderId, recipientId]
+      );
+
+      await db.none(
+        "CALL insert_contact($1, $2)",
+        [senderId, recipientId]
+      );
+  
+      res.status(200).send("Request accepted");
     }
   } catch (err) {
     res.status(500).json(err);
