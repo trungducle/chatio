c314bd004933949a4ff9aacf6dd228732bcf65d9
datnguyenxtiens <dat192001@gmail.com>
2021-11-14 22:03:33 +0700

    Add Create Group Modal UI

--- client/src/components/logoBar/logoBar.jsx
+++ client/src/components/logoBar/logoBar.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateGroup from "../createGroup/CreateGroup";
 import "./logoBar.css";
 
 const LogoBar = () => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <div className="logoBar">
       <img src={require('./chatio.png')} alt="Chatio Logo" className="logo"/>
-      <div className="new-conversation">
+      <div 
+        className="new-conversation" 
+        onClick={() => {
+          setOpenModal(true)
+        }}
+      >
         <span className="material-icons">add_circle</span>
       </div>
+      {openModal && <CreateGroup closeModal={setOpenModal}/>}
     </div>
   );
 }
